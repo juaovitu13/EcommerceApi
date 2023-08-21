5f9579245173e35031b99072d52ba7c0d983201c
juaovitu13 <joao.victorsoares@outlook.com>
2023-08-21 16:12:27 -0300

    Cors configurada

--- APIBookstore/Program.cs
+++ APIBookstore/Program.cs
@@ -8,6 +8,19 @@ builder.Services.AddDbContext<ProductContext>(options =>
  options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddControllers();
+
+// Adicione a configuração do CORS aqui
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("EnableCORS", builder =>
+    {
+        builder.AllowAnyOrigin() // Permita solicitações de qualquer origem
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .Build();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
